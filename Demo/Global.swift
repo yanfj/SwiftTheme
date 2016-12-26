94c37903c10f1feac7933040105ac7d4f822c1cd
Ge Sen <wxxsw2@gmail.com>
2016-12-26 12:32:19 +0800

    demo-global can expressible by literal

--- Demo/Global.swift
+++ Demo/Global.swift
@@ -6,9 +6,9 @@
 //  Copyright © 2016年 Gesen. All rights reserved.
 //
 
-let globalBackgroundColorPicker = ThemeColorPicker(colors: "#fff", "#fff", "#fff", "#292b38")
-let globalTextColorPicker = ThemeColorPicker(colors: "#000", "#000", "#000", "#ECF0F1")
+let globalBackgroundColorPicker: ThemeColorPicker = ["#fff", "#fff", "#fff", "#292b38"]
+let globalTextColorPicker: ThemeColorPicker = ["#000", "#000", "#000", "#ECF0F1"]
 
 let globalBarTextColors = ["#FFF", "#000", "#FFF", "#FFF"]
 let globalBarTextColorPicker = ThemeColorPicker.pickerWithColors(globalBarTextColors)
-let globalBarTintColorPicker = ThemeColorPicker(colors: "#EB4F38", "#F4C600", "#56ABE4", "#01040D")
+let globalBarTintColorPicker: ThemeColorPicker = ["#EB4F38", "#F4C600", "#56ABE4", "#01040D"]
