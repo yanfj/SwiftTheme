2e24394fcd020e68f4cfc64c8db766e96a15d5a4
Kevin Cramer <kevinx@sent.com>
2019-04-03 21:34:18 -0400

    Fix crash when ThemeStatePicker value is missing.

--- Source/NSObject+Theme.swift
+++ Source/NSObject+Theme.swift
@@ -47,7 +47,13 @@ extension NSObject {
         
         if let statePicker = picker as? ThemeStatePicker {
             let setState = unsafeBitCast(method(for: sel), to: setValueForStateIMP.self)
-            statePicker.values.forEach { setState(self, sel, $1.value()! as AnyObject, UIControl.State(rawValue: $0)) }
+            statePicker.values.forEach {
+                guard let value = $1.value() else {
+                    print("SwiftTheme WARNING: Missing value for ThemeStatePicker! Selector: \(String(describing: sel))")
+                    return
+                }
+                setState(self, sel, value as AnyObject, UIControl.State(rawValue: $0))
+            }
         }
         
         else if let statusBarStylePicker = picker as? ThemeStatusBarStylePicker {
