8a4a31d1f40e07db38c80b518165df66039f7ae5
Gesen <i@gesen.me>
2021-02-22 11:38:31 +0800

    Fix swift package warning

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftTheme",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v9)],
     products: [
         .library(
             name: "SwiftTheme",
